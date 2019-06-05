69897777a441581bbfeab6d160942bfba10ac6ae
佚名 <michael8090@gmail.com>
2019-06-05 19:59:12 +0800

    add cross linked list

---
+++ src/CrossLinkedList.ts
@@ -0,0 +1,49 @@
+interface ListNode {
+    child?: ListNode;
+    nextSibling?: ListNode;
+}
+
+
+const CrossList = {
+    remove(node: ListNode, parent: ListNode, preSibling?: ListNode) {
+        if (preSibling === undefined) {
+            parent.child = node.nextSibling;
+        } else {
+            preSibling.nextSibling = node.nextSibling;
+        }
+    },
+
+    add(node: ListNode, parent: ListNode, preSibling?: ListNode) {
+        if (preSibling === undefined) {
+            parent.child = node;
+        } else {
+            node.nextSibling = preSibling.nextSibling;
+            preSibling.nextSibling = node;
+        }
+    },
+    /**
+     * walk inside the root, fn is called against every child of root, but not root itself
+     * @param root 
+     * @param fn 
+     */
+    visit(root: ListNode, fn: (node: ListNode, parent: ListNode, preSibling?: ListNode) => void) {
+        let parent = root;
+        let preSibling: ListNode | undefined;
+        let node = root.child;
+        while (node) {
+            // fn may change the node, backup first to assure the visiting is stable
+            const {nextSibling, child} = node;
+            fn(node, parent, preSibling);
+            if (nextSibling !== undefined) {
+                preSibling = node;
+                node = nextSibling;
+            } else {
+                preSibling = undefined;
+                parent = node;
+                node = child;
+            }
+        }
+    }
+}
+
+export {ListNode, CrossList};
